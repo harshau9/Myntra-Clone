42723800b9072d92eb1aabbd88e252a747cc4ca9
SAURAV KUMAR <sk729584@gmail.com>
2022-12-14 22:10:52 +0530

    Update app.jsx

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,11 +1,12 @@
 
 import "./App.css";
 import Navbar from "./components/Authentication/MainPageAndNavbar";
-
+import { ProductPageApi } from './pages/ProductPageFetch/ProductPageApi';
 function App() {
   return (
     <div className="App">
-      <ProductPageApi />
+     
+
     </div>
   );
 }
--- myntra/src/index.js
+++ myntra/src/index.js
@@ -7,13 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
