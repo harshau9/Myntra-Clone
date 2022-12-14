279302628d4d48d8dbdb4ea2402f793df5049ce3
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2022-12-14 16:50:12 +0530

    aded

--- myntra/src/App.js
+++ myntra/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Hotel from './pages/Home&Hotels/Hotel';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Update from './pages/Home&Hotels/Update';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotel' element={<Hotel />} />
+      </Routes> */}
+      {/* <Update /> */}
+      <Hotel />
     </div>
   );
 }
