7ddade3a81d00948b494cb88af93e53fa85c709a
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2022-12-20 09:45:54 +0530

    addde

--- myntra/src/pages/Beauty/Beauty.jsx
+++ myntra/src/pages/Beauty/Beauty.jsx
@@ -35,6 +35,7 @@ const getuserData = async () => {
 export const Beauty = () => {
   const [data, setdata] = useState([]);
   const [value, setValue] = useState("");
+  const [val, setVal] = useState("");
 
   const [boxwidth, setBoxwidth] = useState("");
   const [userData, setUserData] = useState({});
@@ -64,11 +65,11 @@ export const Beauty = () => {
     }
   };
   // const totalPages = Math.floor(data.length / 25);
-  async function getData(page, value) {
+  async function getData(page, value,val) {
     // console.log("30", page);
 
     setLoading(true);
-    if (value === "") {
+    if (value === ""||val==="") {
       await axios
         .get(
           `https://myntradata-39agumdo9-aloki9singh.vercel.app/makeup?_page=${page}&_limit=20`
@@ -82,7 +83,7 @@ export const Beauty = () => {
     } else {
       await axios
         .get(
-          `https://myntradata-39agumdo9-aloki9singh.vercel.app/makeup?_page=${page}&_limit=20&product_type=${value}`
+          `https://myntradata-39agumdo9-aloki9singh.vercel.app/makeup?_page=${page}&_limit=20&product_type=${value}&brand=${val}`
         )
         .then((res) => {
           setLoading(false);
@@ -92,11 +93,11 @@ export const Beauty = () => {
     }
   }
   useEffect(() => {
-    getData(page, value);
+    getData(page, value,val);
     getuserData()
       .then((res) => setUserData(res))
       .catch((err) => console.log(err));
-  }, [page, value]);
+  }, [page, value,val]);
   console.log(data);
 
   const handlePage = (val) => {
@@ -269,6 +270,116 @@ export const Beauty = () => {
                 </Box>
               </CheckboxGroup>
             </Box>
+            {/* 2nd */}
+
+            <Box border={"1px solid lightgray"} p="10px">
+              <Text color={"black"} textAlign={"start"} mt="5px" mb="10px">
+                <Text as="b">BRAND</Text>
+              </Text>
+              {/* onChange={setValue} value={value} */}
+              <CheckboxGroup>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("nyx");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">NYX</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("dior");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Dior</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("iman");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">IMAN</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("benefit");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Benefit</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("smashbox");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">SmashBox</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("physicians formula");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Physicians Formula</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("pure anada");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Pure Anada</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("maybelline");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Maybelline</Text>
+                  </Checkbox>
+                </Box>
+                <Box textAlign={"start"}>
+                  <Checkbox
+                    defaultChecked={false}
+                    onChange={() => {
+                      setVal("annabelle");
+                    }}
+                    colorScheme="red"
+                  >
+                    <Text color="black">Annabelle</Text>
+                  </Checkbox>
+                </Box>
+              </CheckboxGroup>
+            </Box>
+            {/* / */}
           </Box>
 
           {/* right */}
