43fb968f59eb470b9e8e2d151f5faa71a93a708e
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-02-09 11:08:24 +0530

    add

--- myntra/src/pages/ThemeContext/ThemeContext.jsx
+++ myntra/src/pages/ThemeContext/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext(null);
 
 const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState("dark");
+    const [theme, setTheme] = useState("light");
     const toggleTheme = () => {
         setTheme((curr) => (curr === "light" ? "dark" : "light"));
     }
